ca3e972b99addfaa5ff1539cb83d03d80e08b182
FelipeVStrehle <felipe.strehle@estudante.ufjf.br>
2026-01-29 01:28:09 -0300

    Links funcionando nos créditos

--- Cenas/tela_creditos.gd
+++ Cenas/tela_creditos.gd
@@ -7,3 +7,7 @@ func _ready():
 func _on_btn_voltar_pressed():
 	# Volta para o menu principal
 	get_tree().change_scene_to_file("res://Cenas/menu_principal.tscn")
+
+
+func _on_rich_text_label_meta_clicked(meta: Variant) -> void:
+	OS.shell_open(str(meta)) # Replace with function body.
