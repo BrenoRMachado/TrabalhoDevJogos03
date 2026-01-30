77e1111810b3683aa5048bafa96a1f8147d1c136
FelipeVStrehle <felipe.strehle@estudante.ufjf.br>
2026-01-30 13:14:18 -0300

    Sons de Hover

--- Scripts/tela_creditos.gd
+++ Scripts/tela_creditos.gd
@@ -1,5 +1,7 @@
 extends Control
 
+@onready var sfx_hover = $SfxHover
+
 func _ready():
 	# Garante que o mouse esteja visível
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
@@ -11,3 +13,12 @@ func _on_btn_voltar_pressed():
 
 func _on_rich_text_label_meta_clicked(meta: Variant) -> void:
 	OS.shell_open(str(meta)) # Replace with function body.
+
+# --- FUNÇÃO DO SOM DE HOVER ---
+# Esta é a função que todos os botões vão usar
+func _on_botao_mouse_entered():
+	# Dica Pro: Variar levemente o tom (pitch) deixa o som menos robótico
+	sfx_hover.pitch_scale = randf_range(0.9, 1.1) 
+	
+	# Toca o som
+	sfx_hover.play()
